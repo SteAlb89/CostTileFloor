220591a54ff51f142ed9cdbda6ed8124e4ddbcef
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-06-12 20:55:00 +0200

    changed variable names

--- CostTileFloor/Program.cs
+++ CostTileFloor/Program.cs
@@ -11,21 +11,19 @@ namespace CostTileFloor
 
             if (userInput == "1")
             {
-
-
                 Console.Write("Please enter the width: ");
                 int width = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Please enter the length: ");
                 int length = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Please enter how many units you need: ");
-                int unit = Convert.ToInt32(Console.ReadLine());
+                int unitCount = Convert.ToInt32(Console.ReadLine());
                 Console.Write($"The total price for units is:  $ ");
-                Console.WriteLine(width * length * unit);
+                Console.WriteLine(width * length * unitCount);
 
-                int rectangle = width * length;
+                int rectangleArea = width * length;
 
                 int hourPrice = 86;
-                int priceLabor = (width * length * rectangle * hourPrice);
+                int priceLabor = (width * length * rectangleArea * hourPrice);
                 Console.WriteLine($"The total cost of labor is : $ {priceLabor}  ");
             }
             else
@@ -40,10 +38,10 @@ namespace CostTileFloor
                 Console.Write($"The total price for units is:  $ ");
                 Console.WriteLine(width * length * unit);
 
-                int triangle = (width * length) / 2;
+                int triangleArea = (width * length) / 2;
 
                 int hourPrice = 86;
-                int priceLabor = (width * length * triangle * hourPrice);
+                int priceLabor = (width * length * triangleArea * hourPrice);
                 Console.WriteLine($"The total cost of labor is : $ {priceLabor}  ");
 
 
