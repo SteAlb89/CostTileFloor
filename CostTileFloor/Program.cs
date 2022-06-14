3c58e46038bcc9414374bb77a6cad989f7ce8d7a
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-06-14 21:32:19 +0200

    remove wrong calculation

--- CostTileFloor/Program.cs
+++ CostTileFloor/Program.cs
@@ -6,47 +6,27 @@ namespace CostTileFloor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1: Rectangle  2: Triangle");
+
+            Console.WriteLine("Choose the shape of room - 1: Rectangle  2: Triangle");
             string userInput = Console.ReadLine();
+
+            Console.Write("Please enter the width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter the length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
 
             if (userInput == "1")
             {
-                Console.Write("Please enter the width: ");
-                int width = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter the length: ");
-                int length = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter how many units you need: ");
-                int unitCount = Convert.ToInt32(Console.ReadLine());
-                Console.Write($"The total price for units is:  $ ");
-                Console.WriteLine(width * length * unitCount);
-
-                int rectangleArea = width * length;
-
-                int hourPrice = 86;
-                int priceLabor = (width * length * rectangleArea * hourPrice);
-                Console.WriteLine($"The total cost of labor is : $ {priceLabor}  ");
+                double rectangleArea = Convert.ToDouble(width * length);
+                Console.WriteLine($"The total cost of labor is : $ {rectangleArea}  ");
             }
             else
             {
+                double triangleArea = Convert.ToDouble(width * length) / 2;
+                Console.WriteLine($"The total cost of labor is : $ {triangleArea}  ");
 
-                Console.Write("Please enter the width: ");
-                int width = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter the length: ");
-                int length = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter how many units you need: "); ;
-                int unit = Convert.ToInt32(Console.ReadLine());
-                Console.Write($"The total price for units is:  $ ");
-                Console.WriteLine(width * length * unit);
-
-                int triangleArea = (width * length) / 2;
-
-                int hourPrice = 86;
-                int priceLabor = (width * length * triangleArea * hourPrice);
-                Console.WriteLine($"The total cost of labor is : $ {priceLabor}  ");
-
-
-            }
-         
+            }      
         }
-    }  
+    }
 }
